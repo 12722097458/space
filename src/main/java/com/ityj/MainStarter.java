9f1f31622f21dd899d5cf6536a075f34fa460515
YinJun <ayinjun1109@163.com>
2026-03-27 11:38:41 +0800

    飞书 - 多维表格

--- src/main/java/com/ityj/MainStarter.java
+++ src/main/java/com/ityj/MainStarter.java
@@ -3,10 +3,18 @@ package com.ityj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MainStarter {
     public static void main(String[] args) {
         SpringApplication.run(MainStarter.class, args);
     }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
 }
